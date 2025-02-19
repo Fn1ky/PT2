6dd2c45900b5a4c0e8b5e65af9e2ea8e494f5be3
Fn1ky <MarkInbox23@gmail.com>
2025-02-19 23:42:24 +0100

    Update pt2.py

--- pt2.py
+++ pt2.py
@@ -1,1 +1,19 @@
+import tkinter as tk
 
+# function to draw lines
+
+def draw(event):
+x1, y1 = (event.x - 1), (event.y -1) #Starting Point
+x2, y2 = (event.x  +1), (event.y +1) #Ending Point
+
+# Set up the main window
+
+root = tk.Tk()
+root.title("Simple drawing tool")
+
+# Creates a canvas widget where you draw
+
+canvas = tk.Canvas(root, width=500, height=500, bg="white")
+canvas.pack()
+
+# Mouse movement
