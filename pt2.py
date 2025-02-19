b90614926a9363c6018a2640ae97197482d4b0c8
Fn1ky <MarkInbox23@gmail.com>
2025-02-20 00:17:34 +0100

    Update pt2.py

--- pt2.py
+++ pt2.py
@@ -17,3 +17,14 @@ canvas = tk.Canvas(root, width=500, height=500, bg="white")
 canvas.pack()
 
 # Mouse movement
+canvas.bind("<B1-Motion>", draw)
+canvas.bind("<Stop Drawing>",  lambda event: None)
+
+# Delete Button
+def clear_canvas():
+canvas.delete("all")
+clear_button = tk.Button(root, text="Clear Canvases", command=clear_canvas)
+clear_button.pack()
+
+# Drawing Libary Loop
+root.mainloop()
